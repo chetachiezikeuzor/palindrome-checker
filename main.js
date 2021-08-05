42ffc1e05c6f2214c5f870591814cf6744b18d16
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-08-04 22:26:03 -0500

    🔖 Added primary functions

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("inputButton").addEventListener("click", function () {
+  var text = document.getElementById("textInput").value;
+  console.log(text);
+  // this is to replace all spaces and special characters in the input string
+  var strChange = text
+    .replace(/ /g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+
+  // this is to split the string into single strings.
+  var splitTheString = strChange.split("");
+
+  // this is to reverse these single strings
+  var reverseTheArray = splitTheString.reverse();
+
+  // this is to join the reversal back together.
+  var joinTheArray = reverseTheArray.join("");
+
+  // boolean to see if the changed string is the same as the reversal
+  var results = document.getElementById("checker");
+  if (strChange === joinTheArray) {
+    results.innerHTML = `<text>🥳 <em>"${text}"</em> id a palindrome</text>`;
+  } else if (strChange !== joinTheArray) {
+    results.innerHTML = `<text>🥸 <em>"${text}"</em> is not a palindrome</text?`;
+  } else {
+    results.innerHTML = `😵‍💫 Something went wrong`;
+  }
+});
